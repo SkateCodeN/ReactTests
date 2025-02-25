22977c7c983819fba1b02b336cb8bfeccf1eddca
GR <immemo1105@gmail.com>
2025-02-24 22:52:55 -0800

    added filters

--- src/component/FilterBar.jsx
+++ src/component/FilterBar.jsx
@@ -1,21 +1,50 @@
-const FilterBar =() =>{
+const FilterBar =({ tasks,handleStateChange}) =>{
 
+     // when the user clicks the filter section for displaying all the completed 
+    //tasks
+    const getCompletedTasks = () =>{
+        const completedTasks = tasks.filter( (task) => task.completed);
+        handleStateChange(completedTasks);
+    }
+
+    //display all the active only aka the ones where its not completed
+    const getActiveTasks= () => {
+        const activeTasks = tasks.filter( (task) => !task.completed);
+        handleStateChange(activeTasks);
+    }
+
+    // Get all the tasks
+    const getAllTasks = () => {
+        handleStateChange(tasks);
+    }
     return (
         <div style={styles.filterContainer} >
             <h5>Filters</h5>
                 <ul style={styles.removeDeco}>
                     <li>
-                        <button type="button">
+                        <button 
+                            type="button"
+                            //onClick={handleAllClick} 
+                            onClick={getAllTasks}
+                        >
                             A
                         </button>
                     </li>
                     <li>
-                        <button type="button">
+                        <button 
+                            type="button"
+                            //onClick={handleActiveClick}
+                            onClick={getActiveTasks}
+                        >
                             Ac
                         </button>
                     </li>
                     <li>
-                        <button type="button">
+                        <button 
+                            type="button"
+                            //onClick={handleCompleteClick}
+                            onClick={getCompletedTasks}
+                        >
                             C
                         </button>
                     </li>
--- src/component/To-DoList.jsx
+++ src/component/To-DoList.jsx
@@ -3,17 +3,13 @@ import Task from './Task';
 import CompletedTaskList from "./CompletedTaskList";
 import FilterBar from "./FilterBar";
 
-const testdata = [
-    {id:"1234", name: "task1", completed:false},
-    {id:"21234", name: "task2", completed:false},
-    {id:"31341", name: "task3", completed:false},
-    {id:"4134", name: "task4", completed:false},
-]
+
 const ToDoList = () => {
     const [tasks, setTasks] = useState([]);
-    const [newTask, setNewTask] =useState("");
+    const [newTask, setNewTask] = useState("");
     const [completedTasks, setCompletedTasks] = useState([]);
 
+    // Handle when the user clicks the add button
     const handleTaskAdd =() =>{
         if(!newTask) return;
         
@@ -28,10 +24,12 @@ const ToDoList = () => {
         setNewTask("")
     }
 
+    //keeps track of the user input on the textbox
     const handleNewTaskInput =(e) =>{
         setNewTask(e.target.value)
     }
 
+    // Handles deleting a task based on its id
     const updateOnTaskDelete =(id) =>{
         const newTaskList = tasks.filter( (task) =>{
             if(task.id != id){
@@ -41,28 +39,42 @@ const ToDoList = () => {
         setTasks(newTaskList);
     }
 
+    //if the user clicks that the task is complete we
+    // update the task to update its bool and update state
+    // for both the task list and completed list
     const updateOnTaskComplete = (id) =>{
         const newTaskList = tasks.filter( (task) =>{
             if(task.id === id){
                 task.completed = true;
-                setCompletedTasks((oldVal) => [...oldVal,task])
+                //setCompletedTasks((oldVal) => [...oldVal,task])
             }
             return task;
         })
         setTasks(newTaskList);
         
     }
+
+    const handleTaskStateChange = (newTask) =>{
+        setCompletedTasks(newTask);
+
+    }
+
+   
+
     if (!tasks) return <p>Loading Tasks...</p>
 
     return (
         <>
             <h3>Your Tasks:</h3>
             <div>
-                <FilterBar />
+                <FilterBar 
+                    
+                    handleStateChange={handleTaskStateChange}
+                    tasks={tasks}
+                />
             </div>
             <ol>
                 {
-                   
                     tasks.map( (taskObj) => ( 
                         <li key ={taskObj.id}>
                             <Task 
